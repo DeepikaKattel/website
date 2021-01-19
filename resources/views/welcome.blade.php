923c8c1ec7785de808c962eeefa02625409ee734
Deepika <deepika.kattel80@gmail.com>
2021-01-19 16:45:32 +0545

    Added services section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
         <!-- Styles -->
         <link href="{{asset('css/welcome.css')}}">
@@ -23,11 +24,16 @@
 
     </head>
     <body>
+
+
     <section>
-        <div class="col-lg-6 col-12">
+        <div class="col-lg-6 col-12" >
             @include('partials.navbar')
             <h1 class="home wow fadeInLeft" data-wow-duration="2s" data-wow-delay="1s">Let IT Grow</h1>
-            <h2 class="wow fadeInLeft" data-wow-duration="2s" data-wow-delay="2s">Your Enterprise...</h2>
+            <h2 class="wow fadeInLeft" data-wow-duration="2s" data-wow-delay="2s">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </h2>
+
         </div>
         <div class="col-lg-6 wow fadeInUp">
             <main id="main" class="background-flowers">
@@ -36,27 +42,76 @@
                 </figure>
             </main>
         </div>
+
     </section>
 
 
-    <section class="about">
-        <div class="col-lg-2 col-12">
-            <h1>About Us</h1>
+    <section class="container about">
+        <div class="row aboutImg" style="justify-content: center">
+            <div class="col-lg-2 col-12 aboutHeading">
+                <h1>About Us</h1>
+            </div>
+            <div class="col-lg-10 col-12 d-flex justify-content-center text-center">
+                <div class="aboutContent wow bounceInRight" data-wow-duration="3s" data-wow-delay="0.5s">
+                    <h1>Why US?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        <br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-10 col-12 aboutImg" >
-            <div class="aboutContent wow bounceInRight" data-wow-duration="4s" data-wow-delay="0.5s">
-                <p style="color:white">sdfsdfdsfasdfdsa<br>afsdfsfsd</p>
+    </section>
+    <section class="services">
+        <div class="col-lg-6 col-12">
+            <main id="main" class="servicesImg">
+                <h1>Services</h1>
+            </main>
+        </div>
+        <div class="col-lg-6 col-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col" id="service1">
+                        <img class="m-3" src="{{asset('images/design.PNG')}}"/>
+                        <h3>Web Design</h3>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                            Bonorum et Malorum for use in a type specimen book.
+                             Bonorum et Malorum for use in a type specimen book.</p>
+                    </div>
+                    <div class="col" id="service2">
+                        <img class="m-3" src="{{asset('images/development.PNG')}}"/>
+                        <h3> Web Development</h3>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                            inibus Bonorum et Malorum for use in a type specimen book.
+                            inibus Bonorum et Malorum for use in a type specimen book.</p>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col" id="service3">
+                        <img class="m-3" src="{{asset('images/browser.PNG')}}"/>
+                        <h3>Hosting Services</h3>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                            inibus Bonorum et Malorum for use in a type specimen book.
+                           norum et Malorum for use in a type specimen book.</p>
+                    </div>
+                    <div class="col" id="service4">
+                        <img class="m-3" src="{{asset('images/mobile-app.PNG')}}"/>
+                        <h3>Mobile Application Development</h3>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                            inibus Bonorum et Malorum for use in a type specimen book.
+                            onorum et Malorum for use in a type specimen book.</p>
+                    </div>
+                </div>
             </div>
         </div>
 
-    </section>
-    <section class="products">
+
+
 
     </section>
     <section class="team">
     </section>
     <script src="{{asset('js/nav.js')}}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
     {{-- Wow js --}}
     <script src="{{asset('js/wow.min.js')}}"></script>
     <script>
