c1cf6d5818f232b3612b09f50634ed35694fa007
Deepika <deepika.kattel80@gmail.com>
2021-01-20 21:20:42 +0545

    Made changes in background and add products section

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+    <div class="col-lg-12 col-12 mt-3">
+        <div class="col-lg-4 col-12 text-center mt-2">
+            <h3>Why Choose Us?</h3>
+            <hr/>
+            <p>Lorem ipsumsdasdsadasd Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit,consequat.
+                Lorem ipsumsdasdsadasd Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit,consequat</p>
+        </div>
+        <div class="col-lg-4 col-12 text-center mt-2">
+            <h3>Quick Links</h3>
+            <hr/>
+            <ul class="list-unstyled mt-5">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Team</a></li>
+
+            </ul>
+        </div>
+        <div class="col-lg-4 col-12 text-center mt-2">
+            <h3>Connect With Us</h3>
+            <hr/>
+            <ul class="text-center mt-5">
+                <li class="list-inline-item"><a href=""><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item"><a href=""><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href=""><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href=""><i class="fa fa-google-plus"></i></a></li>
+                <li class="list-inline-item"><a href="" target="_blank"><i class="fa fa-envelope"></i></a></li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <p><b>Copyright - Let IT Grow Pvt. Ltd.</b></p>
+        </div>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
         <link href="{{asset('css/welcome.css')}}">
@@ -24,90 +24,123 @@
 
     </head>
     <body>
+    <section class="container-fluid">
+        <div class="row aboutImg justify-content-center">
+            <div class="col-lg-12 col-12 p-0 m-0">
+                <div class="col-lg-6 col-12">
+                    @include('partials.navbar')
+                    <h1 class="home wow fadeInLeft" data-wow-duration="2s" data-wow-delay="1s">Let IT Grow</h1>
+                    <h2 class="wow fadeInLeft" data-wow-duration="2s" data-wow-delay="2s">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </h2>
 
-
-    <section>
-        <div class="col-lg-6 col-12" >
-            @include('partials.navbar')
-            <h1 class="home wow fadeInLeft" data-wow-duration="2s" data-wow-delay="1s">Let IT Grow</h1>
-            <h2 class="wow fadeInLeft" data-wow-duration="2s" data-wow-delay="2s">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </h2>
-
-        </div>
-        <div class="col-lg-6 wow fadeInUp">
-            <main id="main" class="background-flowers">
-                <figure class="swing" style="float:right">
-                    <img src="{{asset('images/logoFull.png')}}"/>
-                </figure>
-            </main>
+                </div>
+                <div class="col-lg-6 wow fadeInUp background-flowers" id="main">
+                    <figure class="swing float-right">
+                        <img src="{{asset('images/logoFull.png')}}"/>
+                    </figure>
+                </div>
+            </div>
         </div>
 
     </section>
 
 
     <section class="container about">
-        <div class="row aboutImg" style="justify-content: center">
-            <div class="col-lg-2 col-12 aboutHeading">
-                <h1>About Us</h1>
+        <div class="row aboutImg justify-content-center">
+            <div class="col-lg-12 col-12 wow fadeIn">
+                <div class="col-lg-2 col-12 wow rotateIn">
+                    <h1 class="aboutHeading">About Us</h1>
+                </div>
+                <div class="col-lg-10 aboutContentImg col-12 wow fadeIn">
+                    <div class="wow aboutContent bounceInRight" data-wow-duration="2s" data-wow-delay="1s">
+                        <h1>Why US?</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            <br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+
             </div>
-            <div class="col-lg-10 col-12 d-flex justify-content-center text-center">
-                <div class="aboutContent wow bounceInRight" data-wow-duration="3s" data-wow-delay="0.5s">
-                    <h1>Why US?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        <br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </section>
+    <section class="container services">
+        <div class="row justify-content-center">
+            <h1 class="wow fadeIn" data-wow-duration="2s" data-wow-delay="2s">Services</h1>
+            <div class="col-lg-12 col-12 p-0 m-0">
+                <div class="col-lg-6 col-12 wow slideInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
+{{--                    <h2 class="wow fadeIn" data-wow-duration="2s" data-wow-delay="2.5s">We provide all kind of services as per the requirements of our clients.</h2>--}}
+                    <main id="main" class="servicesImg">
+
+                    </main>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="container">
+                        <div class="row flex-container">
+                            <div class="col wow fadeInUp flex-item" data-wow-duration="3s" data-wow-delay="1s" id="service1">
+                                <img class="m-3" src="{{asset('images/design.PNG')}}"/>
+                                <h3>Web Design</h3>
+                                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                                    Bonorum et Malorum for use in a type specimen book.
+                                    </p>
+                            </div>
+                            <div class="col wow fadeInUp flex-item" data-wow-duration="3s" data-wow-delay="1.5s" id="service2">
+                                <img class="m-3" src="{{asset('images/development.PNG')}}"/>
+                                <h3> Web Development</h3>
+                                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                                    inibus Bonorum et Malorum for use in a type specimen book.
+                                    </p>
+                            </div>
+{{--                            <div class="w-100"></div>--}}
+                            <div class="col wow fadeInUp flex-item" data-wow-duration="3s" data-wow-delay="2s" id="service3">
+                                <img class="m-3" src="{{asset('images/browser.PNG')}}"/>
+                                <h3>Hosting Services</h3>
+                                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                                    inibus Bonorum et Malorum for use in a type specimen book.
+                                   </p>
+                            </div>
+                            <div class="col wow fadeInUp flex-item" data-wow-duration="3s" data-wow-delay="2.5s" id="service4">
+                                <img class="m-3" src="{{asset('images/mobile-app.PNG')}}"/>
+                                <h3>Mobile Application Development</h3>
+                                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
+                                    inibus Bonorum et Malorum for use in a type specimen book.
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
-    <section class="services">
-        <div class="col-lg-6 col-12">
-            <main id="main" class="servicesImg">
-                <h1>Services</h1>
-            </main>
-        </div>
-        <div class="col-lg-6 col-12">
-            <div class="container">
-                <div class="row">
-                    <div class="col" id="service1">
-                        <img class="m-3" src="{{asset('images/design.PNG')}}"/>
-                        <h3>Web Design</h3>
-                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
-                            Bonorum et Malorum for use in a type specimen book.
-                             Bonorum et Malorum for use in a type specimen book.</p>
+    <section class="container products">
+        <div class="row justify-content-center">
+            <h1>Our Products</h1>
+            <div class="col-lg-12 col-12  p-0 m-0">
+                <div class="col-lg-4 col-12" id="cardImg">
+                    <div class="card text-center">
+                        <img class="mx-auto mt-5 d-block" src="{{asset('images/settings.PNG')}}"/>
+                        <h2>Product 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit,consequat.</p>
                     </div>
-                    <div class="col" id="service2">
-                        <img class="m-3" src="{{asset('images/development.PNG')}}"/>
-                        <h3> Web Development</h3>
-                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
-                            inibus Bonorum et Malorum for use in a type specimen book.
-                            inibus Bonorum et Malorum for use in a type specimen book.</p>
+                </div>
+                <div class="col-lg-4 col-12" id="cardImg">
+                    <div class="card text-center">
+                        <img class="mx-auto mt-5 d-block" src="{{asset('images/settings.PNG')}}"/>
+                        <h2>Product 2</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit,consequat.</p>
                     </div>
-                    <div class="w-100"></div>
-                    <div class="col" id="service3">
-                        <img class="m-3" src="{{asset('images/browser.PNG')}}"/>
-                        <h3>Hosting Services</h3>
-                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
-                            inibus Bonorum et Malorum for use in a type specimen book.
-                           norum et Malorum for use in a type specimen book.</p>
-                    </div>
-                    <div class="col" id="service4">
-                        <img class="m-3" src="{{asset('images/mobile-app.PNG')}}"/>
-                        <h3>Mobile Application Development</h3>
-                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
-                            inibus Bonorum et Malorum for use in a type specimen book.
-                            onorum et Malorum for use in a type specimen book.</p>
+                </div>
+                <div class="col-lg-4 col-12" id="cardImg">
+                    <div class="card text-center">
+                        <img class="mx-auto mt-5 d-block" src="{{asset('images/settings.PNG')}}"/>
+                        <h2>Product 3</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit,consequat.</p>
                     </div>
                 </div>
             </div>
         </div>
-
-
-
-
     </section>
-    <section class="team">
-    </section>
+    @include('partials.footer')
     <script src="{{asset('js/nav.js')}}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
